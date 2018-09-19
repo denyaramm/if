d5e46829d41888aed8740049b555b08961f96d21
denyaramm <denya1987@inbox.ru>
2018-09-19 13:18:33 +0600

    проект

---
+++ src/IF.java
@@ -0,0 +1,18 @@
+public class IF {
+    public static void main(String[] args) {
+        int age = 50;
+        if (age == 25) {
+            System.out.println("Tima");
+            if (age == 33) {
+                System.out.println("Vasya");
+                if (age == 50) {
+                    System.out.println("Denis");
+                }
+            }
+        } else {
+            System.out.println("Не верно");
+
+        }
+    }
+}
+
